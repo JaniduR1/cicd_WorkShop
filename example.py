e68048376d1fb67fe5e79bfebcaf563cee017b88
Janidu <jr798@canterbury.ac.uk>
2022-12-06 16:55:04 +0000

    My First CI Container test

---
+++ example.py
@@ -0,0 +1,9 @@
+def add(a, b):
+    return (
+        a + b
+    )  # This is a really long string meaning nothing, test for black . -- This is a really long string meaning nothing, test for black . --
+    # BOOOO! This Bad
+
+
+def multiply(a, b):
+    return a * b
---
+++ test_example.py
@@ -0,0 +1,13 @@
+from example import *
+
+
+def test_add():
+    assert add(1, 2) == 3
+    assert add(1, -2) == -1
+    assert add(1000000000000000000, 2) == 1000000000000000002
+
+
+def test_multiply():
+    assert multiply(1, 2) == 2
+    assert multiply(1, -2) == -2
+    # assert False, "please implement the rest of this test"
